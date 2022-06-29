926c5175b7c7e5f733376fcf266e07e66abae8fb
Jura Prokofev <Smyrke2005@yandex.ru>
2022-06-29 17:13:37 +0300

    First upload Temperature sensor

---
+++ Temperature_Sensor/include/main.h
@@ -0,0 +1,33 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+//----------- DEFINES --------------------------------------------------------------------
+#define DEVICE_NAME							"BMP180_Temperature_Sensor"
+// #define MODEL_NAME							"ESP8266/ESP32"
+#if defined(ARDUINO_ARCH_ESP8266)
+	#define MODEL_NAME						"ESP8266"
+#elif defined(ARDUINO_ARCH_ESP32)
+	#define MODEL_NAME						"ESP32"
+#endif
+#define SLEEP_SECONDS						900
+#define FIRMWARE_VERSION					"1.0."
+#define FIRMWARE_VERSION_BUFF_SIZE			14
+
+//----------- INCLUDES --------------------------------------------------------------------
+// #include <esp_functions.h>
+
+//----------- STRUCTIRES -------------------------------------------------------------------
+
+//----------- VARIABLES --------------------------------------------------------------------
+extern char firmvare_version[ FIRMWARE_VERSION_BUFF_SIZE ];
+
+//----------- INCLUDES ---------------------------------------------------------------------
+
+//----------- FUNCTIONS---------------------------------------------------------------------
+void my_homekit_report(const double temperature);
+
+
+
+
+#endif //_MAIN_H_
---
+++ Temperature_Sensor/src/main.cpp
@@ -0,0 +1,84 @@
+#include "main.h"
+#include <Arduino.h>
+#include <arduino_homekit_server.h>
+#ifdef BMP180_SENSOR
+	#include <BMP180.h>
+	#include <Wire.h>
+#endif
+#include <timer.h>
+#include <esp_functions.h>
+
+//----------- DEFINES ----------------------------------------------------------------------
+
+
+//----------- VARIABLES --------------------------------------------------------------------
+char firmvare_version[ FIRMWARE_VERSION_BUFF_SIZE ];
+#ifdef BMP180_SENSOR
+	BMP180 bmp180( BMP180_ULTRAHIGHRES );
+#endif
+Timer timer0( 0, 1000 );
+
+#ifdef SLEEP_MODE
+	uint8_t sec_count = 0;
+#endif
+
+// access your homekit characteristics defined in my_accessory.c
+extern "C" homekit_server_config_t config;
+extern "C" homekit_characteristic_t cha_current_temperature;
+
+//----------- FUNCTIONS--------------------------------------------------------------------
+void setup()
+{
+	Serial.begin( 115200 );
+#ifdef BMP180_SENSOR
+	bmp180.begin();
+#endif
+
+	strcpy( firmvare_version, FIRMWARE_VERSION );
+	strcat( firmvare_version, FIRMWARE_REVISION );
+
+	esp::init();
+
+	esp::wifi_STA_init( DEVICE_NAME );
+
+	arduino_homekit_setup( &config );
+#ifdef SLEEP_MODE
+	system_deep_sleep_set_option( 1 );
+#endif
+}
+
+//-----------------------------------------------------------------------------------------
+void loop()
+{
+	arduino_homekit_loop();
+
+	if( timer0.isInterrupt() ){
+		timer0.confirmInerrupt();
+
+		if( arduino_homekit_connected_clients_count() ){
+#ifdef BMP180_SENSOR
+			my_homekit_report( bmp180.getTemperature() );
+#endif
+#ifdef SLEEP_MODE
+			if( sec_count++ >= 10 ){
+				system_deep_sleep_instant( SLEEP_SECONDS * 1000 );
+				sec_count = 0;
+			}
+#endif
+		}
+
+		ESP_DEBUG("Free heap: %d, HomeKit clients: %d\n", ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+	}
+}
+
+//-----------------------------------------------------------------------------------------
+void my_homekit_report( const double temperature )
+{
+	cha_current_temperature.value.float_value = temperature;
+	ESP_DEBUG( "Temperature: %.2f\n", temperature );
+	homekit_characteristic_notify( &cha_current_temperature, cha_current_temperature.value );
+}
+
+//-----------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------
